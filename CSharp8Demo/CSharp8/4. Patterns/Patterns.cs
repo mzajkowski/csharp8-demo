5a408af54ff1e98994b96c24869fae5ed09fdb58
Marcin Zajkowski <m.zajkowski@sicc.pl>
2019-04-04 02:32:18 +0200

    Patterns demo

---
+++ CSharp8Demo/CSharp8/4. Patterns/Patterns.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CSharp8.Patterns
+{
+    [Description("[4] Patterns")]
+    class Patterns
+    {
+        static void Main(string[] args)
+        {
+            #region Tuple Pattern
+            var p1 = new Person("Kim", "Sneum", "Madsen");
+            var p2 = new Person("Marcin", "Zajkowski");
+
+            WritePersonDetailsFor(p1);
+            WritePersonDetailsFor(p2);
+            #endregion
+        }
+
+        private static void WritePersonDetailsFor(Person p)
+        {
+            switch (p.FirstName, p.MiddleName, p.LastName)
+            {
+                case (string fn, string mn, string ln):
+                    Console.WriteLine($"{fn} {mn} {ln}");
+                    return;
+
+                case (string fn, null, string ln):
+                    Console.WriteLine($"{fn} {ln}");
+                    return;
+
+                case (string fn, null, null):
+                    Console.WriteLine($"{fn}");
+                    return;
+
+                // etc...
+            }
+
+            #region Switch Expressions
+            //var result = (p.FirstName, p.MiddleName, p.LastName) switch
+            //{
+            //    (string fn, string mn, string ln) => $"{fn} {mn} {ln}",
+            //    (string fn, null, string ln) => $"{fn} {ln}",
+            //    (string fn, null, null) => $"{fn}"
+            //};
+
+            //Console.WriteLine(result);
+            #endregion
+        }
+    }
+}
